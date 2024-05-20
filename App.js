e5b5736800be33855fd6a7d318fc591a061ebf92
raja_choudhary <raja@choudhary.com>
2024-05-20 18:36:36 +0530

    created success

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>this is rajesh kumar's app don't touch it!</Text>
+      <Text>this is rajesh kumar's app don't touch it!</Text>
       <StatusBar style="auto" />
     </View>
   );
